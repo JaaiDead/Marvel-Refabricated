2da2c85dea17a7722b6eaea2ef398dfecf635d21
JaaiDead <arcticquests.dev@gmail.com>
2025-01-26 00:06:53 +0530

    Add Files Again......

---
+++ src/client/java/org/arcticquests/dev/client/MarvelSuperHeroesClient.java
@@ -0,0 +1,10 @@
+package org.arcticquests.dev.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarvelSuperHeroesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/arcticquests/dev/MarvelSuperHeroes.java
@@ -0,0 +1,10 @@
+package org.arcticquests.dev;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MarvelSuperHeroes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
